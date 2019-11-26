b9e7e8c3092fa20ddd37f7cb72434ed30cdce838
Josh West <josh.west@centre.edu>
2019-11-26 11:10:06 -0500

    progress on population data; check values

--- javascript.js
+++ javascript.js
@@ -1,15 +1,21 @@
 //Load in geoJSON data
 var mapPromise = d3.json("data/us-states_5m.json");
+//Load in national employment data based on occupation
 //var csEmploymentPromise = ;
+//Load in state employment rates 2018-2020
 var csShortTermPromise = d3.csv("data/CS Job Short Term Projections Data.csv");
+//Load in state employment rates 2016-2026
+var csLongTermPromise = d3.csv("data/CS Job Long Term Projections Data.csv");
+//Load in national women's employment data
 //var womenPromise = ;
+//Load in national minorites' employment data
 //var minoritiesPromise = ;
-//var popPromise = ;
+var popPromise = d3.csv("data/sc-est2018-alldata6.csv");
 
 mapPromise.then(function(mapData)
 {
     console.log("map data working",mapData);
-    setup(mapData); //do i need to add mapData to this???
+    setup(mapData);
    
 },
 function(err)
@@ -23,7 +29,7 @@ var setup = function(mapData) // svg size, projection
     var height = 800;
     //Define projection
     var projection = d3.geoAlbersUsa().translate([width/2,height/2]).scale([1000]);
-    console.log("projection",projection)
+    //console.log("projection",projection)
     
     //make svg
     var svg = d3.select("svg")
@@ -43,9 +49,44 @@ var setup = function(mapData) // svg size, projection
 }   
 
 //draw pathgenerator and d3 core algorithm
-var drawMap = function()
+var drawMap = function() // this may be needed for added animations to the graph
 {
-    //does path generator go here?
     //d3 core algorithm
-    
+}
+
+popPromise.then(function(popData)
+{
+    console.log("state population data working",popData);
+    getData(popData);
+},
+function(err){console.log("ERROR",err)})
+
+var hash = {} //this will be used for joining in the future
+
+
+var getData = function(popData)
+{
+    states = [{name: "Alabama", male: [], female: [], all: [], white: [], black: [], asian: [], multiracial: []}] //each array within each state object will need to be summed; I will also have to copy paste the Alabama object for each state
+    popData.forEach(function(d)
+    {
+        //var state = d.name;
+        console.log(d.AGE);//sets the var state to the name of the state so that state number can be selected in the following if statements
+        if(d.AGE == 40) //this is the average age according to https://datausa.io/profile/cip/computer-science-6
+            {if(d.SEX = 0)//
+                {states[d.STATE-1].all.push(d.POPESTIMATE2017)} //console log what each of these are
+//             if(d.sex= 1)//male
+//                {states[d.state-1].male.push(d.POPESTIMATE2017)}
+//             if(d.sex = 2)//female
+//                {states[d.state-1].female.push(d.POPESTIMATE2017)}
+//             if(d.race = 1)//white
+//                {states[d.state-1].white.push(d.POPESTIMATE2017)}
+//             if(d.race = 2)//black
+//                {states[d.state-1].black.push(d.POPESTIMATE2017)}
+//             if(d.race = 4)//asian
+//                {states[d.state-1].asian.push(d.POPESTIMATE2017)}
+//             if(d.race = 6) //two or more races
+//                {states[d.state-1].multiracial.push(d.POPESTIMATE2017)}
+            }
+    }) ;
+    console.log(states)
 }
